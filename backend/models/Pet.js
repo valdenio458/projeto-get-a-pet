eaf2c5f42e03f553b50a555892ea39e7d3a62587
valdenio458 <valdeniomartinho@gmail.com>
2022-05-28 09:30:33 -0300

    :heavy_check_mark: feat: cria model de pet

---
+++ backend/models/Pet.js
@@ -0,0 +1,38 @@
+import mongoose from "../db/conn";
+const { Schema } = mongoose;
+
+const Pet = mongoose.model(
+  'Pet',
+  new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+  },
+    age: {
+      type: Number,
+      required: true,
+  },
+  weight: {
+    type: Number,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: Array,
+    required: true,
+  },
+  available: {
+    type: Boolean,      
+},
+  user: Object,
+  adopter: Object
+  },
+    { timestamps: true }, // cria createdAt, updatedAt
+  ),
+);   
+
+export default Pet;
